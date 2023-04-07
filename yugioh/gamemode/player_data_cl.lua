b8995ef5df51eb61604ee62ddb1103c0e2e05308
62yg <103649248+62yg@users.noreply.github.com>
2023-04-07 18:36:04 +0100

    Delete yugioh/gamemode directory

--- yugioh/gamemode/player_data_cl.lua
+++
@@ -1,7 +0,0 @@
--- player_data_cl.lua (client)
-if CLIENT then
-net.Receive("InitializePlayerDeck", function()
-    LocalPlayer().yugiohDeck = net.ReadTable()
-    LocalPlayer().yugiohTrunk = net.ReadTable()
-end)
-end
